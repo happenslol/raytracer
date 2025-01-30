33c396b4c83a06a375f3dcd7286dcfb0d4eba98c
Hilmar Wiegand <me@hwgnd.de>
2025-01-30 22:14:28 +0100

    Make generic hit interface

--- src/main.zig
+++ src/main.zig
@@ -6,30 +6,136 @@ const stbImageWrite = @cImport({
 const Vec3 = @import("vec3.zig");
 const Ray = @import("ray.zig");
 
-fn hitSphere(center: *const Vec3, radius: f64, r: *const Ray) f64 {
-    const oc = center.sub(r.origin);
-    const a = r.direction.lengthSquared();
-    const h = r.direction.dot(oc);
-    const c = oc.lengthSquared() - radius * radius;
+const HitRecord = struct {
+    p: Vec3,
+    normal: Vec3,
+    t: f64,
+    front_face: bool,
 
-    const discriminant = h * h - a * c;
-
-    // 0 or > 0 means 1 solution (tangent) or 2 solutions (intersection)
-    if (discriminant < 0) {
-        return -1;
+    fn init() HitRecord {
+        return .{
+            .p = Vec3.init(0, 0, 0),
+            .normal = Vec3.init(0, 0, 0),
+            .t = 0,
+            .front_face = false,
+        };
     }
 
-    return (h - @sqrt(discriminant)) / a;
-}
+    /// Sets the hit record normal vector.
+    // NOTE: the parameter `outward_normal` is assumed to have unit length.
+    fn setFaceNormal(self: *HitRecord, r: *const Ray, outward_normal: Vec3) void {
+        self.front_face = r.direction.dot(outward_normal) < 0;
+        self.normal = if (self.front_face) outward_normal else outward_normal.mulScalar(-1);
+    }
+};
 
-const sphereCenter = Vec3.init(0.0, 0.0, -1.0);
-const sphereRadius = 0.5;
+const Hittable = struct {
+    ptr: *const anyopaque,
+    hitFn: *const fn (ptr: *const anyopaque, r: *const Ray, ray_tmin: f64, ray_tmax: f64, hit_record: ?*HitRecord) bool,
 
-fn rayColor(r: Ray) Vec3 {
-    const t = hitSphere(&sphereCenter, sphereRadius, &r);
-    if (t > 0) {
-        const n = r.at(t).sub(Vec3.init(0.0, 0.0, -1.0)).normalize();
-        return Vec3.init(n.x + 1.0, n.y + 1.0, n.z + 1.0).mulScalar(0.5);
+    fn init(ptr: anytype) Hittable {
+        const T = @TypeOf(ptr);
+        const ptr_info = @typeInfo(T);
+
+        const gen = struct {
+            pub fn hit(self_ptr: *const anyopaque, r: *const Ray, ray_tmin: f64, ray_tmax: f64, hit_record: ?*HitRecord) bool {
+                const self: T = @ptrCast(@alignCast(self_ptr));
+                return @call(.always_inline, ptr_info.Pointer.child.hit, .{ self, r, ray_tmin, ray_tmax, hit_record });
+            }
+        };
+
+        return .{
+            .ptr = ptr,
+            .hitFn = gen.hit,
+        };
+    }
+
+    pub inline fn hit(self: Hittable, r: *const Ray, ray_tmin: f64, ray_tmax: f64, hit_record: ?*HitRecord) bool {
+        return self.hitFn(self.ptr, r, ray_tmin, ray_tmax, hit_record);
+    }
+};
+
+const Sphere = struct {
+    center: Vec3,
+    radius: f64,
+
+    fn init(center: Vec3, radius: f64) Sphere {
+        return .{ .center = center, .radius = @max(0, radius) };
+    }
+
+    fn hit(self: *const Sphere, r: *const Ray, ray_tmin: f64, ray_tmax: f64, hit_record: ?*HitRecord) bool {
+        const oc = self.center.sub(r.origin);
+        const a = r.direction.lengthSquared();
+        const h = r.direction.dot(oc);
+        const c = oc.lengthSquared() - self.radius * self.radius;
+
+        // 0 or > 0 means 1 solution (tangent) or 2 solutions (intersection)
+        const discriminant = h * h - a * c;
+
+        if (discriminant < 0) {
+            return false;
+        }
+
+        const discriminantSqrt = @sqrt(discriminant);
+
+        // Find the nearest root that lies in the acceptable range.
+        var root = (h - discriminantSqrt) / a;
+        if (root < ray_tmin or ray_tmax < root) {
+            root = (h + discriminantSqrt) / a;
+            if (root <= ray_tmin or ray_tmax <= root)
+                return false;
+        }
+
+        if (hit_record) |rec| {
+            rec.t = root;
+            rec.p = r.at(root);
+            rec.setFaceNormal(r, rec.p.sub(self.center).divScalar(self.radius));
+        }
+
+        return true;
+    }
+};
+
+const HittableList = struct {
+    objects: std.ArrayList(Hittable),
+
+    pub fn init(allocator: std.mem.Allocator) HittableList {
+        return .{ .objects = std.ArrayList(Hittable).init(allocator) };
+    }
+
+    pub fn add(self: *HittableList, obj: Hittable) !void {
+        try self.objects.append(obj);
+    }
+
+    pub fn clear(self: *HittableList) void {
+        self.objects.clearRetainingCapacity();
+    }
+
+    pub fn deinit(self: *HittableList) void {
+        self.objects.deinit();
+    }
+
+    pub fn hit(self: *const HittableList, r: *const Ray, ray_tmin: f64, ray_tmax: f64, hit_record: ?*HitRecord) bool {
+        var temp_rec = HitRecord.init();
+        var hit_anything = false;
+        var closest_so_far = ray_tmax;
+
+        for (self.objects.items) |obj| {
+            if (obj.hit(r, ray_tmin, closest_so_far, &temp_rec)) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                hit_record.?.* = temp_rec;
+            }
+        }
+
+        return hit_anything;
+    }
+};
+
+fn rayColor(r: Ray, world: *HittableList) Vec3 {
+    var rec = HitRecord.init();
+    if (world.hit(&r, 0, std.math.floatMax(f64), &rec)) {
+        return rec.normal.add(Vec3.init(1.0, 1.0, 1.0)).mulScalar(0.5);
     }
 
     const unit_direction = r.direction.normalize();
@@ -55,8 +161,14 @@ pub fn main() !void {
     const comp = 3;
     const stride = image_width * comp;
 
-    // Camera
+    // World
+    var world = HittableList.init(allocator);
+    defer world.deinit();
 
+    try world.add(Hittable.init(&Sphere.init(Vec3.init(0.0, 0.0, -1.0), 0.5)));
+    try world.add(Hittable.init(&Sphere.init(Vec3.init(0, -100.5, -1.0), 100)));
+
+    // Camera
     const focal_length = 1.0;
     const viewport_height = 2.0;
     const viewport_width = viewport_height * (@as(f64, @floatFromInt(image_width)) / @as(f64, @floatFromInt(image_height)));
@@ -96,7 +208,7 @@ pub fn main() !void {
             const ray_direction = pixel_center.sub(camera_center);
             const ray = Ray.init(camera_center, ray_direction);
 
-            rayColor(ray).writePixel(data, (j * image_width + i) * comp);
+            rayColor(ray, &world).writePixel(data, (j * image_width + i) * comp);
         }
     }
 
